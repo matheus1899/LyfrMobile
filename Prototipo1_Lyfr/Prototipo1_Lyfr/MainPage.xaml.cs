b8d298e54c90a773c8cb6a6ec1837ecc9e1cdd45
Matheus Tenorio <matheus.santos1899@gmail.com>
2019-06-26 18:16:30 -0300

    Adicionar arquivos de projeto.

---
+++ Prototipo1_Lyfr/Prototipo1_Lyfr/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Prototipo1_Lyfr
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ContentPage_SizeChanged(object sender, EventArgs e)
+        {
+            var a = sender as ContentPage;
+            if (a.Width > a.Height)
+            {
+                bxv1.HeightRequest = bxv1.Height * 2.5;
+                bxv2.HeightRequest = bxv2.Height * 2.5;
+            }
+        }
+
+        private void ChamarPagCadastrar(object sender, EventArgs e)
+        {
+            return;
+        }
+    }
+}
