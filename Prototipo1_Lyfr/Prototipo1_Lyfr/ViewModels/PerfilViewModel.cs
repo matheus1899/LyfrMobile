6435df0f3bcf55748aa9062f61dcce3a00406a38
Matheus Tenorio <matheus.santos153@senaisp.edu.br>
2019-09-19 17:14:41 -0300

    Atualizado a View Perfil para um layout mais agradável;

--- Prototipo1_Lyfr/Prototipo1_Lyfr/ViewModels/PerfilViewModel.cs
+++ Prototipo1_Lyfr/Prototipo1_Lyfr/ViewModels/PerfilViewModel.cs
@@ -6,6 +6,14 @@ namespace Prototipo1_Lyfr.ViewModels
     {
         private string _Plano;
         private string _Nome;
+        private string _Telefone;
+        private string _Rua;
+        private string _Numero;
+        private string _Cidade;
+        private string _Estado;
+        private string _Email;
+        private string _Senha;
+
         public string Plano
         {
             get
@@ -28,11 +36,95 @@ namespace Prototipo1_Lyfr.ViewModels
                 SetProperty<string>(ref _Nome, value, nameof(Nome));
             }
         }
+        public string Telefone
+        {
+            get
+            {
+                return _Telefone;
+            }
+            set
+            {
+                SetProperty<string>(ref _Telefone, value, nameof(Telefone));
+            }
+        }
+        public string Rua
+        {
+            get
+            {
+                return _Rua;
+            }
+            set
+            {
+                SetProperty<string>(ref _Rua, value, nameof(Rua));
+            }
+        }
+        public string Numero
+        {
+            get
+            {
+                return _Numero;
+            }
+            set
+            {
+                SetProperty<string>(ref _Numero, value, nameof(Numero));
+            }
+        }
+        public string Cidade
+        {
+            get
+            {
+                return _Cidade;
+            }
+            set
+            {
+                SetProperty<string>(ref _Cidade, value, nameof(Cidade));
+            }
+        }
+        public string Estado
+        {
+            get
+            {
+                return _Estado;
+            }
+            set
+            {
+                SetProperty<string>(ref _Estado, value, nameof(Estado));
+            }
+        }
+        public string Email
+        {
+            get
+            {
+                return _Email;
+            }
+            set
+            {
+                SetProperty<string>(ref _Email, value, nameof(Email));
+            }
+        }
+        public string Senha
+        {
+            get
+            {
+                return _Senha;
+            }
+            set
+            {
+                SetProperty<string>(ref _Senha, value, nameof(Senha));
+            }
+        }
 
         public PerfilViewModel(Cliente cliente)
         {
             Plano = cliente.Plano == "P" ? "Premium" : "Gratuito";
             Nome = cliente.Nome;
+            Nome = cliente.Nome;
+            Rua = cliente.Rua;
+            Numero = cliente.Numero;
+            Cidade = cliente.Cidade;
+            Estado = cliente.Estado;
+            Email = cliente.Email;
+            Telefone = cliente.Telefone;
         }
     }
 }
