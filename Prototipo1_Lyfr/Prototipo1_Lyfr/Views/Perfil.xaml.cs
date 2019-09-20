94df1843bd529075a9900d91c686f31115e247b1
Matheus Tenorio <matheus.santos153@senaisp.edu.br>
2019-09-20 02:35:09 -0300

    Atualizações na View Perfil; Adicionado novas imagens para teste;

--- Prototipo1_Lyfr/Prototipo1_Lyfr/Views/Perfil.xaml.cs
+++ Prototipo1_Lyfr/Prototipo1_Lyfr/Views/Perfil.xaml.cs
@@ -18,15 +18,12 @@ namespace Prototipo1_Lyfr.Views
         {
             InitializeComponent();
         }
-
         public Perfil(Cliente cliente)
         {
             InitializeComponent();
             this.BindingContext = new ViewModels.PerfilViewModel(cliente);
             _cliente = cliente;
         }
-
-
         private async void EnviarSugestao_Clicked(object sender, System.EventArgs e)
         {
             Sugestao sugestao = new Sugestao()
@@ -53,6 +50,32 @@ namespace Prototipo1_Lyfr.Views
             catch (Exception ex)
             {
                 MostrarMensagem.Mostrar(ex.Message);
+            }
+        }
+        private void ContentPage_SizeChanged(object sender, EventArgs e)
+        {
+            MudarEstadoImagem();
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            MudarEstadoImagem();
+        }
+        private void MudarEstadoImagem()
+        {
+            if (Width > Height)
+            {
+                VisualStateManager.GoToState(img_lidos, "Landscape");
+                VisualStateManager.GoToState(img_plan, "Landscape");
+                bxv1.IsVisible = false;
+                bxv2.IsVisible = false;
+            }
+            else
+            {
+                VisualStateManager.GoToState(img_lidos, "Portrait");
+                VisualStateManager.GoToState(img_plan, "Portrait");
+                bxv1.IsVisible = true;
+                bxv2.IsVisible = true;
             }
         }
     }
