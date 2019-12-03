97b8af20dd66d560ad67843c19d9b4ff66ea5541
MiliaGnvs <milia.pg02@gmail.com>
2019-12-03 16:52:28 -0300

    Adicionando subcapítulos;

--- Prototipo1_Lyfr/Prototipo1_Lyfr/Conexao/ManagerEpub.cs
+++ Prototipo1_Lyfr/Prototipo1_Lyfr/Conexao/ManagerEpub.cs
@@ -15,7 +15,7 @@ namespace Prototipo1_Lyfr.Conexao
     public class ManagerEpub
     {
         ConexaoAPI conexao = new ConexaoAPI();
-        public List<Capitulos> list_capitulos{ get; set; }
+        public List<Capitulos> list_capitulos { get; set; }
 
         public static EpubBook epubBook;
         EpubContent bookContent;
@@ -23,29 +23,27 @@ namespace Prototipo1_Lyfr.Conexao
         public static string capitulo;
         Livros _livro;
 
-        public  ManagerEpub(Livros Livro)
+        public ManagerEpub(Livros Livro)
         {
-            this._livro = Livro;
-            
+            this._livro = Livro;       
             GetLivroFromCache();
         }
 
-        public void GetLivroFromCache()
+        public async void GetLivroFromCache()
         {
             string caminho = GetLivroCache(_livro.Arquivo, _livro.Titulo);
             FileStream fs = File.Open(caminho, FileMode.Open);
 
-            epubBook =  EpubReader.ReadBook(fs);
+            epubBook = await  EpubReader.ReadBookAsync(fs);
             bookContent = epubBook.Content;
         }
 
-        public static string GetLivroCache(string link, string titulo)
+        public string GetLivroCache(string link, string titulo)
         {
             var diretorio = "storage/emulated/0/Android/data/com.the_endeavour.lyfr/files/" + titulo.Replace(" ", "") + ".epub";
             if (!Directory.Exists(diretorio))
             {
-                System.Net.WebClient client = new System.Net.WebClient();
-                client.DownloadFile(link, diretorio);
+                DownloadFile(link, diretorio);
                 return diretorio;
             }
             else
@@ -54,12 +52,19 @@ namespace Prototipo1_Lyfr.Conexao
             }
         }
 
+        public void DownloadFile(string link, string diretorio)
+        {
+            System.Net.WebClient client = new System.Net.WebClient();
+            Uri uri = new Uri(link);
+            client.DownloadFileAsync(uri, diretorio);
+        }
+
         public List<Capitulos> LoadChapter()
         {
             list_capitulos = new List<Capitulos>();
             foreach (EpubChapter chapter in epubBook.Chapters)
             {
-                list_capitulos.Add(new Capitulos() { Title = chapter.Title, TargetType = typeof(Leitor)});
+                list_capitulos.Add(new Capitulos() { Title = chapter.Title, TargetType = typeof(Leitor), SubChapters = chapter.SubChapters});
             }
             return list_capitulos;
         }
@@ -70,6 +75,7 @@ namespace Prototipo1_Lyfr.Conexao
             foreach (EpubChapter chapter in epubBook.Chapters)
             {
                 string htmlcontent = chapter.HtmlContent;
+
                 if (chapter.Title == capitulo)
                 {
                     htmlWeb.Html += htmlcontent;
--- Prototipo1_Lyfr/Prototipo1_Lyfr/Models/Capitulos.cs
+++ Prototipo1_Lyfr/Prototipo1_Lyfr/Models/Capitulos.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using VersFx.Formats.Text.Epub;
 
 namespace Prototipo1_Lyfr.Models
 {
     public class Capitulos
     {
         public string Title { get; set; }
+        public List<EpubChapter> SubChapters { get; set; }
         public Type TargetType { get; set; }
     }
 }
